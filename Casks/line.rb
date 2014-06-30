3d0c7b4f761a075bfa8809712b923776d5b98833
renshu <nantra168@gmail.com>
2014-06-30 23:59:26 +0900

    add line.rb

---
+++ Casks/line.rb
@@ -0,0 +1,10 @@
+class Line < Cask
+  version 'latest'
+  sha256 :no_check
+
+  url 'http://dl.desktop.line.naver.jp/naver/LINE/osx/Line.dmg'
+  homepage 'http://line.me/'
+
+  link 'Line.app'
+end
+
